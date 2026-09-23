77501d4d4982766cf6395d22885c4898e896c597
ajanani2k7-anantha <ajanani2k7@gmail.com>
2026-09-23 22:10:25 +0530

    Update task1_data_collection.py

--- task1_data_collection.py
+++ task1_data_collection.py
@@ -1,1 +1,12 @@
+import requests
 
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+
+headers = {
+    "User-Agent": "TrendPulse/1.0"
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.status_code)
+print(response.json()[:10])
